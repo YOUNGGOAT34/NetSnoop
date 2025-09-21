3d9f4a309aab633ec104ac81485efd0476308b1d
GOAT <lucasonyiego283@gmail.com>
2025-09-21 16:50:02 +0300

    error utility working correctly

--- netsnoop.c
+++ netsnoop.c
@@ -2,12 +2,12 @@
 
 void error(bool with_exit,const i8* error_message){
     if(with_exit){
-       fprintf(stderr,"%s:(%s)\n",error_message,strerror(errno));
+       fprintf(stderr,RED"%s:(%s)\n"RESET,error_message,strerror(errno));
        exit(EXIT_FAILURE);
 
     }
 
-    fprintf(stderr,"%s:(%s)\n",error_message,strerror(errno));
+    fprintf(stderr,RED"%s:(%s)\n"RESET,error_message,strerror(errno));
    
 
 }
