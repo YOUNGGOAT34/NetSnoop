def61b4b4848b935cb5ee6807bf36f3f50f74471
GOAT <lucasonyiego283@gmail.com>
2025-09-21 16:25:44 +0300

    unsigned data types

--- netsnoop.h
+++ netsnoop.h
@@ -6,10 +6,18 @@
 #include <strings.h>
 #include <stdbool.h>
 
+//signed data types
 typedef char i8;
 typedef signed short int i16;
 typedef signed int i32;
 typedef signed long int i64;
+
+
+//unsigned data types
+typedef unsigned char u8;
+typedef unsigned short int u16;
+typedef unsigned int u32;
+typedef unsigned long int u64;
 
 
 
