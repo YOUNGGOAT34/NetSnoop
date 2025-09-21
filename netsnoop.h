71cf27e1784d996eed80115e9b72de7a5617c675
GOAT <lucasonyiego283@gmail.com>
2025-09-21 16:46:42 +0300

    defining the terminal output colors

--- netsnoop.h
+++ netsnoop.h
@@ -9,6 +9,14 @@
 
 #define BUFFER_SIZE 65536
 
+//colors
+#define RED   "\033[1;31m"
+#define GREEN   "\033[1;32m"
+#define YELLOW "\033[1;33m"
+#define RESET "\033[0m"
+#define CYAN "\033[1;36m"
+#define WHITE "\033[1;37m"
+
 //signed data types
 typedef char i8;
 typedef signed short int i16;
